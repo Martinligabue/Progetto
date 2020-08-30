04b827167d34bce1bd5b7d6e80ccce2dea6a976d
Martin Ligabue <martinligabue@gmail.com>
2020-08-30 16:11:36 +0200

    todo

--- src/mp/progetto/Shop.java
+++ src/mp/progetto/Shop.java
@@ -3,8 +3,7 @@ package mp.progetto;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Shop {// manage games and users, manages also usernames/ids univocity (no that should
-					// be managed by user.java)
+public class Shop {// todo:add the default prices as default, also map?
 	static ArrayList<String> gameList = new ArrayList<>(Arrays.asList("Game1", "Game2", "Game3", "Game4"));
 	static Double[] array = { 0.0, 10.0, 50.0, 24.6 };
 	static ArrayList<Double> prices = new ArrayList<Double>(Arrays.asList(array));
