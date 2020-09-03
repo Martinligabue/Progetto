ab65cf8c0a392050cd41ac7a165ebfad1d17371e
Martin Ligabue <martinligabue@gmail.com>
2020-09-03 17:38:17 +0200

    test

--- src/mp/progetto/UserTest.java
+++ src/mp/progetto/UserTest.java
@@ -27,22 +27,23 @@ public class UserTest extends Streams {
 		Game game1 = new Game("Spider", priceOfGame(shop.getGameList(), "Spider"));
 		User primoUtente = new User.UserBuilder("john88", "John Wick").withAge(20).setGames(game1, shop.getGameList())
 				.build();
-		
+
 		assertThat(primoUtente.getId()).isEqualTo("john88");
 		assertThat(primoUtente.getName()).isEqualTo("John Wick");
 		assertThat(primoUtente.getAge()).isEqualTo(20);
 		assertThat(primoUtente.getGames().contains(game1));
 	}
+
 	@Test
 	public void testCustomList() {
 		shop.clean();
 		Game game1 = new Game("Spider", 10.5);
-		ArrayList<Game> list1=new ArrayList<Game>();
+		ArrayList<Game> list1 = new ArrayList<Game>();
 		list1.add(game1);
-		User primoUtente = new User.UserBuilder("john88", "John Wick").setGames(game1, list1)
-				.build();
+		User primoUtente = new User.UserBuilder("john88", "John Wick").setGames(game1, list1).build();
 		assertThat(primoUtente.getGames().contains(game1));
 	}
+
 	@Test
 	public void testAddGames() {
 		ArrayList<Game> list = new ArrayList<Game>();
@@ -51,12 +52,13 @@ public class UserTest extends Streams {
 		User primoUtente = new User.UserBuilder("john88", "John").setGames(list, shop.getGameList()).build();
 
 		primoUtente.addGame("Freecell", shop.getGameList());
-		
+
 		assertThat(primoUtente.containsName(primoUtente.getGames(), "Spider"));
 		assertThat(primoUtente.containsName(primoUtente.getGames(), "Freecell"));
 		assertThat(!primoUtente.containsName(primoUtente.getGames(), "Pyramid"));
-		
-		User secondoUtente = new User.UserBuilder("bob", "Robert").setGames(new Game ("Klondike", 0), shop.getGameList()).build();
+
+		User secondoUtente = new User.UserBuilder("bob", "Robert").setGames(new Game("Klondike", 0), shop.getGameList())
+				.build();
 		assertThat(secondoUtente.containsName(secondoUtente.getGames(), "Klondike"));
 
 	}
